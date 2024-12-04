f55d0114375284b5a55b9775fc42d7035d5c4bef
basil <basileye@gmail.com>
2024-12-04 18:29:53 +0100

    API infos configuration

--- src/main/java/com/eyelis/messagerouter/configuration/SwaggerConfig.java
+++ src/main/java/com/eyelis/messagerouter/configuration/SwaggerConfig.java
@@ -2,17 +2,28 @@ package com.eyelis.messagerouter.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${api.info.title}")
+    private String title;
+
+    @Value("${api.info.description}")
+    private String description;
+
+    @Value("${api.info.version}")
+    private String version;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Message API")
-                        .version("1.0")
-                        .description("API documentation for the Message application"));
+                        .title(title)
+                        .version(version)
+                        .description(description));
     }
 }
