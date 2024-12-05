f25e9281f0a679252de914f5b7e6d957b0ffce48
basil <basileye@gmail.com>
2024-12-05 17:19:25 +0100

    Add logs and fix send request

--- src/main/java/com/eyelis/messagerouter/infrastructure/adapter/web/MessageController.java
+++ src/main/java/com/eyelis/messagerouter/infrastructure/adapter/web/MessageController.java
@@ -6,12 +6,11 @@ import com.eyelis.messagerouter.application.usecase.ListMessageUseCase;
 import com.eyelis.messagerouter.domain.model.Message;
 import com.eyelis.messagerouter.domain.repository.MessageProducerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +18,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/messages")
 @RequiredArgsConstructor
+@Slf4j
 public class MessageController {
 
     private final CreateMessageUseCase createMessageUseCase;
@@ -31,17 +31,16 @@ public class MessageController {
     private String topic;
 
     @PostMapping
-    public ResponseEntity<Message> createMessage(@RequestParam
-                                 String content,
-                                 @RequestParam
-                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-                                 LocalDateTime timestamp
+    public ResponseEntity<Message> createMessage(@RequestBody
+                                                 Message message
     ) {
-        return ResponseEntity.ok(createMessageUseCase.execute(content, timestamp));
+        log.info(STR."Creating message [\{message}]");
+        return ResponseEntity.ok(createMessageUseCase.execute(message.content(), message.timestamp()));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Message> getMessage(@PathVariable Long id) {
+        log.info(STR."Getting message by id [\{id}] ");
         Optional<Message> message = getMessageUseCase.execute(id);
         return message.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -49,11 +48,13 @@ public class MessageController {
 
     @GetMapping
     public ResponseEntity<List<Message>> listMessages() {
+        log.info("Listing messages");
         return ResponseEntity.ok(listMessageUseCase.execute());
     }
 
     @PostMapping("send")
-    public void sendMessage(@RequestParam String message) {
+    public void sendMessage(@RequestBody String message) {
+        log.info(STR."Sending topic [\{topic}] message [\{message}] ");
         messageProducer.produce(topic, message);
     }
 }
--- src/main/java/com/eyelis/messagerouter/infrastructure/adapter/web/PartnerController.java
+++ src/main/java/com/eyelis/messagerouter/infrastructure/adapter/web/PartnerController.java
@@ -5,6 +5,7 @@ import com.eyelis.messagerouter.application.usecase.GetPartnerUseCase;
 import com.eyelis.messagerouter.application.usecase.ListPartnerUseCase;
 import com.eyelis.messagerouter.domain.model.Partner;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/partners")
 @RequiredArgsConstructor
+@Slf4j
 public class PartnerController {
 
     private final CreatePartnerUseCase createPartnerUseCase;
@@ -26,6 +28,7 @@ public class PartnerController {
     public ResponseEntity<Partner> createPartner(@RequestBody
                                                  Partner partner
     ) {
+        log.info(STR."Creating partner [\{partner}]");
         Partner savedPartner = createPartnerUseCase.execute(partner);
         URI location = URI.create("/api/partners/" + savedPartner.id());
         return ResponseEntity.created(location).body(savedPartner);
@@ -33,6 +36,7 @@ public class PartnerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Partner> getPartner(@PathVariable Long id) {
+        log.info(STR."Getting partner by id [\{id}] ");
         Optional<Partner> partner = getPartnerUseCase.execute(id);
         return partner.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -40,6 +44,7 @@ public class PartnerController {
 
     @GetMapping
     public ResponseEntity<List<Partner>> listPartners() {
+        log.info("Listing partners");
         return ResponseEntity.ok(listPartnerUseCase.execute());
     }
 
